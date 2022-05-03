974fbd13019ae91abb006a4ca4a2086f3ce6f917
c21rebja <c21rebja@student.his.se>
2022-05-03 14:10:43 +0200

    Created shared preferences and showed it in a textview in main activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView viewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        viewName = findViewById(R.id.nameText);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getSharedPreferences("preferences", MODE_PRIVATE);
+        String name = prefs.getString("name", "No name found.");
+        viewName.setText(name);
+
+        //SharedPreferences.Editor edit = prefs.edit();
+    }
 }
